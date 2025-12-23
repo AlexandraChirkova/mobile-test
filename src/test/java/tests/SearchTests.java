597e4b7a1378e132aa0c7ca22dea01aa164451fe
alexandra <alexandra.chirkova@kaspi.kz>
2025-12-23 15:32:21 +0500

    add some new

--- src/test/java/tests/SearchTests.java
+++ src/test/java/tests/SearchTests.java
@@ -3,6 +3,7 @@ package tests;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.CollectionCondition.sizeGreaterThan;
+import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.*;
 import static io.appium.java_client.AppiumBy.*;
 import static io.qameta.allure.Allure.step;
@@ -12,7 +13,7 @@ public class SearchTests extends BaseTest {
     @Test
     void successfulSearchTest() {
         step("Type search", () -> {
-            $(accessibilityId("Search Wikipedia")).click();
+            $(accessibilityId("Search Wikipedia")).shouldBe(visible).click();
             $(id("org.wikipedia.alpha:id/search_src_text")).sendKeys("Appium");
         });
         step("Verify content found", () ->
