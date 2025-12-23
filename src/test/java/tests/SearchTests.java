254815962e4eb3cf97b69c0bd7b02e2ef1987e3a
alexandra <alexandra.chirkova@kaspi.kz>
2025-12-23 16:02:34 +0500

    add new test

--- src/test/java/tests/SearchTests.java
+++ src/test/java/tests/SearchTests.java
@@ -12,12 +12,35 @@ public class SearchTests extends BaseTest {
 
     @Test
     void successfulSearchTest() {
-        step("Type search", () -> {
-            $(accessibilityId("Search Wikipedia")).shouldBe(visible).click();
-            $(id("org.wikipedia.alpha:id/search_src_text")).sendKeys("Appium");
+
+        step("Skip onboarding if it appears", () -> {
+            if ($(id("org.wikipedia.alpha:id/fragment_onboarding_skip_button")).exists()) {
+                $(id("org.wikipedia.alpha:id/fragment_onboarding_skip_button"))
+                        .shouldBe(visible)
+                        .click();
+            }
+
+            if ($(accessibilityId("Skip")).exists()) {
+                $(accessibilityId("Skip"))
+                        .shouldBe(visible)
+                        .click();
+            }
         });
-        step("Verify content found", () ->
-                $$(id("org.wikipedia.alpha:id/page_list_item_title"))
-                        .shouldHave(sizeGreaterThan(0)));
+
+        step("Type search", () -> {
+            $(accessibilityId("Search Wikipedia"))
+                    .shouldBe(visible)
+                    .click();
+
+            var input = $(id("org.wikipedia.alpha:id/search_src_text"));
+            input.shouldBe(visible).click();
+            input.toWebElement().sendKeys("Appium");
+        });
+
+        step("Verify content found", () -> {
+            $$(id("org.wikipedia.alpha:id/page_list_item_title"))
+                    .shouldHave(sizeGreaterThan(0));
+        });
     }
 }
+
